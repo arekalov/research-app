1fb2c58111cfd3b090658a83b245a10f72454dd9
arekalov <artyom.rekalov@gmail.com>
2026-03-08 17:11:03 +0300

    [feat] add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.arekalov.researchapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.arekalov.researchapp"
@@ -37,9 +32,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
---
+++ app/src/main/java/com/arekalov/researchapp/ResearchApp.kt
@@ -0,0 +1,7 @@
+package com.arekalov.researchapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ResearchApp : Application()
